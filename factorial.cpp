12e64b66ad5ead3c2777f3685f8d5389635a11e8
Nyv <79833594+supernyv@users.noreply.github.com>
2024-02-13 00:50:50 +0100

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -4,12 +4,7 @@
 
 int factorial(int n)
 {
-    if(n == 1){
-        return 1;
-    }
-    else{
-        return n*factorial(n-1);
-    }
+    return (n == 1) ? (1) : (n*factorial(n-1));
 }
 int main(){
     int example = 5;
